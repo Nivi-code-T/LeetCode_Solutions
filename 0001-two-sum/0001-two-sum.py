d161c08c2d1553b703586e1a1cc634b233f7cb88
Nivi-code-T <niveteggi@gmail.com>
2025-09-29 11:52:55 -0500

    Time: 2044 ms (33.11%), Space: 13.3 MB (47.85%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,7 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        seen_numbers = {}  # Hash map to store numbers and their indices
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in seen_numbers:
-                return [seen_numbers[complement], i]
-            seen_numbers[num] = i
-        return []
+    def twoSum(self,nums, target):
+        for i in range(len(nums)):
+          for j in range(i+1,len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i,j]
+        return[]
